e8e32fe8479aa4361254ba3fd116ca4cc7ba3e74
Lima Douglas <douglas.pereira@aluno.unifametro.edu.br>
2023-08-29 07:41:25 -0300

    First

--- resources/views/layout/_partials/menus/menu-lateral.blade.php
+++ resources/views/layout/_partials/menus/menu-lateral.blade.php
@@ -1,78 +1,39 @@
 <nav class="mt-2">
-  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-    <li class="nav-item">
-      <a href="{{route('construtoras.canteiros.index', $company->id)}}" class="nav-link">
-        <i class="nav-icon far fa-building"></i>
-        <p>Canteiros</p>
+  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+    <!-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library -->
+    <li class="nav-item menu">
+      <a href="" class="nav-link">
+        <i class="fas fa-clipboard-list fa-lg"></i>
+        <p>
+          PONTO
+          <i class="right fas fa-angle-left"></i>
+        </p>
       </a>
+      <ul class="nav nav-treeview">
+        <li class="nav-item">
+          <a href="" class="nav-link">
+            <i class="fas fa-user-clock fa-lg"></i>
+             <p>Pontos</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">
+            <i class="far fa-file-pdf fa-lg"></i>
+             <p>Relatório</p>
+          </a>
+        </li>
+      </ul>
     </li>
-    <li class="nav-item">
-      <a href="{{route('construtoras.usuarios.index', $company->id)}}" class="nav-link">      
-        <i class="nav-icon fas fa-user-friends"></i>
-        <p>Usuários</p>
-      </a>
-    </li>
-    {{-- <li class="nav-item">   
-      <a href="{{route('construtoras.funcionarios.index', $company->id)}}" class="nav-link">      
-        <i class="nav-icon far fa-address-card"></i>
-        <p>Funcionários</p>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a href="{{route('construtoras.funcoes.index', $company->id)}}" class="nav-link">      
-        <i class="nav-icon fas fa-hand-paper"></i>
-        <p>Funções</p>
+    {{-- <li class="nav-item">
+      <a href="" class="nav-link">
+        <i class="nav-icon fas fa-th"></i>
+        <p>
+          Simple Link
+          <span class="right badge badge-danger">New</span>
+        </p>
       </a>
     </li> --}}
   </ul>
-  
 </nav>
-{{-- <li class="nav-item">
-    <a href="#" class="nav-link">
-        <i class="nav-icon fas fa-hands-helping"></i>
-      <p>
-        Clientes
-        <i class="fas fa-angle-left right"></i>
-      </p>
-    </a>
-    <ul class="nav nav-treeview">
-      <li class="nav-item">
-        <a href="pages/layout/top-nav.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Chamados</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Unidades Habitacionais</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="pages/layout/boxed.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Financeiro</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="pages/layout/fixed-sidebar.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Avisos</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="pages/layout/fixed-sidebar-custom.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Galeria de Imagens</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="pages/layout/fixed-sidebar-custom.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Inconformidades-RNC</p>
-        </a>
-      </li>
-    </ul>
-</li> --}}
-
-
+<!-- /.sidebar-menu -->
