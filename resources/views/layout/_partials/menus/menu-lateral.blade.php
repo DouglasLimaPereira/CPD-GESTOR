228d710b33536e277813e66ebd1f406c0a9a753d
dcapote92 <danicapote92@gmail.com>
2023-09-04 23:04:03 -0300

    jquery

--- resources/views/layout/_partials/menus/menu-lateral.blade.php
+++ resources/views/layout/_partials/menus/menu-lateral.blade.php
@@ -1,9 +1,7 @@
 <nav class="mt-2">
-  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="horaextra" role="menu">
-    <!-- Add icons to the links using the .nav-icon class
-         with font-awesome or any other icon font library -->
+  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="erro" role="menu">
     <li class="nav-item menu">
-      <a href="{{ route('erro-sitef.index') }}" class="nav-link">
+      <a href="{{ route('erro-sitef.index')}}" class="nav-link">
         <i class="nav-icon fas fa-history fa-lg"></i>
         <p>
           Consultar Erro Sitef
