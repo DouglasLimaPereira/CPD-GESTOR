38e965740f59f6e54f3bb19e8e8d4c0fef1090b3
dcapote92 <danicapote92@gmail.com>
2023-10-30 13:48:18 -0300

    asd

--- app/Http/Controllers/Api/CodigoBarraController.php
+++ app/Http/Controllers/Api/CodigoBarraController.php
@@ -9,7 +9,7 @@ class CodigoBarraController extends Controller
 {
     public function gerarCod($codigo){
         // $codigo_ = '<img src="data:image/png;base64,{{DNS1D::getBarcodePNG("'+$codigo+'", "EAN13",1,53,array(1,1,1), true)}}" alt="barcode"/>';
-        $codigo = '<img src="data:image/png;base64,' . DNS1DFacade::getBarcodePNG($codigo, 'EAN13',1.3,65,array(1,1,1), true) . '" alt="barcode"   />';
+        $codigo = '<img src="data:image/png;base64,' . DNS1DFacade::getBarcodePNG($codigo, 'EAN13',1,60,array(1,1,1), true) . '" alt="barcode"   />';
         return response()->json($codigo);
     }
 }
