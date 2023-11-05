f5f0af18c7bc33ce94ec805b8dd9538efebcd508
dcapote92 <danicapote92@gmail.com>
2023-11-04 22:04:26 -0300

    retentativa

--- resources/views/erro-sitef/_form.blade.php
+++ resources/views/erro-sitef/_form.blade.php
@@ -40,6 +40,19 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label for="retentativa">Permite retentativa *</label>
+                <select name="retentativa" id="retentativa" class="form-control">
+                    <option> Selecine </option>
+                    <option value="1" {{isset($erro_sitef) && $erro_sitef->retentativa == 1 ? 'selected' : ''}} > SIM </option>
+                    <option value="0" {{isset($erro_sitef) && $erro_sitef->retentativa == 0 ? 'selected' : ''}} > NÃO </option>
+                </select>
+            </div>
+        </div>
+    </div>
+
     <hr>
     <a href="{{route('erro-sitef.index')}}" class="btn btn-sm btn-secondary"><i class="fas fa-undo-alt"></i> CANCELAR</a>
     <button type="submit" class="btn btn-sm btn-success">{!!(isset($erro_sitef)) ? '<i class="fas fa-sync"></i> ATUALIZAR' : '<i class="fas fa-save"></i> SALVAR'!!}</button>
