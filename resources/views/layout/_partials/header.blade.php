04b5eb3cb0c144fded5d67628e0b5443ac212e3b
Douglas de Lima Pereira <douglas.pereira@aluno.unifametro.edu.br>
2023-11-08 11:28:12 -0300

    style checkbox

--- resources/views/layout/_partials/header.blade.php
+++ resources/views/layout/_partials/header.blade.php
@@ -17,10 +17,8 @@
   <link rel="stylesheet" href="{{asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- iCheck -->
   <link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
-
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
 
   <!-- JQVMap -->
@@ -37,4 +35,21 @@
   {{-- Toastr --}}
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <style>
+    .status {
+      width: 1.5rem; 
+      height: 1.5rem; 
+      background-color: green;
+      border-radius: .6rem;
+      border:1px solid green;
+    }
+    .status input:checked {
+      background-color: #002B4E;
+    }
+    .status:after {
+      content: "";
+      position: absolute;
+      display: none;
+    }
+  </style>
 </head>
