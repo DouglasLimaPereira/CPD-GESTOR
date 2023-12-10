654e1447c53fd686111f0aee269f25d6dadcb596
Douglas Lima <douglas.pereira@aluno.unifametro.edu.br>
2023-12-10 07:45:47 -0300

    Merge

--- database/seeders/FilialTableSeeder.php
+++ database/seeders/FilialTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Filial;
 
-class FilialSeeder extends Seeder
+class FilialUserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
