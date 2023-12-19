3dbfa479d4baf368711fd925696047c734365db5
dcapote92 <danicapote92@gmail.com>
2023-12-19 20:51:01 -0300

    reajuste migrate escala

--- database/seeders/EscalaTableSeeder.php
+++ database/seeders/EscalaTableSeeder.php
@@ -22,30 +22,35 @@ class EscalaTableSeeder extends Seeder
                 'evento' => 'DSR',
                 'data_inicio' => '2023-09-07 07:00:00',
                 'data_fim' => '2023-09-07 22:00:00',
+                'color' => '#ff9884'
             ],
             [
                 'user_id' => 1,
                 'evento' => 'FOLGA',
                 'data_inicio' => '2023-09-10 07:00:00',
                 'data_fim' => '2023-09-10 22:00:00',
+                'color' => '#ff9884'
             ],
             [
                 'user_id' => 1,
                 'evento' => 'DSR',
                 'data_inicio' => '2023-09-14 07:00:00',
                 'data_fim' => '2023-09-14 22:00:00',
+                'color' => '#ff9884'
             ],
             [
                 'user_id' => 1,
                 'evento' => 'DSR',
                 'data_inicio' => '2023-09-21 07:00:00',
                 'data_fim' => '2023-09-21 22:00:00',
+                'color' => '#ff9884'
             ],
             [
                 'user_id' => 1,
                 'evento' => 'DSR',
                 'data_inicio' => '2023-09-28 07:00:00',
                 'data_fim' => '2023-09-28 22:00:00',
+                'color' => '#ff9884'
             ]
         ];
 
