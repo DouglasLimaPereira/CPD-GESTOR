c2bd4d26eafd3820d0119d0e471a806d331da6e4
Douglas Lima <douglas.pereira@aluno.unifametro.edu.br>
2024-01-10 17:35:01 -0300

    removendo arquivos obsoletos

--- CPD-GESTOR/resources/views/layout/_partials/menus/menu-top.blade.php
+++
@@ -1,9 +0,0 @@
-<ul class="navbar-nav">
-    <li class="nav-item">
-       <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars fa-lg"></i></a>  
-    </li>
-    <li class="nav-item">
-        <span class="nav-link" style="color: black">Sistema de Gestão & Controle / <a href="{{ route('filial.index', session()->get('filial')->id) }}" style="text-decoration:none; color: black; cursor: pointer;">SM{{session()->get('filial')->codigo}} - {{session()->get('filial')->bairro}}</a></span>
-    </li>
-</ul>
-
--- CPD-GESTOR/resources/views/layout/_partials/preloader.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- Preloader -->
-<div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{asset('assets/images/SIC.png')}}" alt="SGC" height="35" width="35">
-</div>
