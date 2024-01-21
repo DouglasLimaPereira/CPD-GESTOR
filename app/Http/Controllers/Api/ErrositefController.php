4ce3e911df54836ff98fa4fedeab8be28cc8bccd
Douglas Lima <douglas.pereira@alumo.unifametro.edu.br>
2024-01-21 08:22:49 -0300

    logica consulta erro sitef reajuste

--- app/Http/Controllers/Api/ErrositefController.php
+++ app/Http/Controllers/Api/ErrositefController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 class ErrositefController extends Controller
 {
     public function consultar($cod_erro){
+        if($cod_erro <= 9){
+            if ($cod_erro[0] != 0) {
+                $cod_erro = '0'.$cod_erro;
+            }
+        } 
+
         $erro = Errositef::firstWhere('codigo', $cod_erro);
         return response()->json($erro);
     }
